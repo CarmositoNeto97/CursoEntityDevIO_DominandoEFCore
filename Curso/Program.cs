a8dee01121e42dfa072e078021cb3e9a3422ceba
CarmositoNeto97 <carmozikaneto@outlook.com>
2022-12-22 17:26:43 -0300

    first commit

---
+++ Curso/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DominandoEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EnsureCreatedAnDeleted();
+        }
+
+        static void EnsureCreatedAnDeleted()
+        {
+            using var db = new Curso.Data.ApplicationContext();
+            db.Database.EnsureCreated();
+        }
+    }
+}
